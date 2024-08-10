24b0fa5600d54ea90917f754892b79c904beb744
NabeekhAbid <nabeekh@gmail.com>
2024-08-11 02:35:46 +0500

    onEarth and NavBars UI implemented

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,20 +1,29 @@
 import React from "react"
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 import "./styling/Navbar.css"
 
 const Navbar = () => {
+  const location = useLocation()
+
   return (
     <nav className="navbar">
-      <div className="navbar-logo">
-        <img src="./functions-insurance/assets/logo.webp" alt="logo" />
+      <div className="navbar-logo" style={{ color: 'rgb(18, 14, 107)' }}>
+        {location.pathname === '/earth/openearth' && (
+          <h2>Cervet's Asset Library</h2>
+        )}
       </div>
       <ul className="navbar-links">
-        <li>
-          <a href="/sign-in">Sign In</a>
+        {/* <li>
+          <Link to="/sign-in">Sign In</Link>
         </li>
         <li>
-          <a href="/">Settings</a>
-        </li>
+          <Link to="/">Settings</Link>
+        </li> */}
+
+        <div className="navbar-logo">
+          <img src="/avatar.webp" alt="logo" />
+        </div>
+
       </ul>
     </nav>
   )
--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,32 +1,40 @@
-import React from "react"
-import { Link } from "react-router-dom"
-import "./styling/Sidebar.css"
+import React from "react";
+import { Link } from "react-router-dom";
+import "./styling/Sidebar.css";
+import Icon from '../components/styling/icon';
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
-      <div className="logo">
-        <img src="/favicon-a.png" alt="logo" />
-      </div>
       <ul className="sidebar-links">
         <li>
-          <Link to="/">Propose a Contract</Link>
+          <Link to="/">
+            <Icon sx={{ color: 'black', fontSize: 15, mr: 5 }} icon={'mdi:home'} />
+          </Link>
         </li>
         <li>
-          <Link to="/accept">Accept a Contract</Link>
+          <Link to="/accept">
+            <Icon sx={{ color: 'black', fontSize: 15, mr: 5 }} icon={'mdi:abc'} />
+          </Link>
         </li>
         <li>
-          <Link to="/earth/portfolio">Portfolio</Link>
+          <Link to="/earth/portfolio">
+            <Icon sx={{ color: 'black', fontSize: 15, mr: 5 }} icon={'mdi:earth'} />
+          </Link>
         </li>
         <li>
-          <Link to="/earth/openearth">Open Earth</Link>
+          <Link to="/earth/openearth">
+            <Icon sx={{ color: 'black', fontSize: 15, mr: 5 }} icon={'mdi:ac-unit'} />
+          </Link>
         </li>
         <li>
-          <Link to="/earth/assetlibrary">Asset Library</Link>
+          <Link to="/earth/assetlibrary">
+            <Icon sx={{ color: 'black', fontSize: 15, mr: 5 }} icon={'mdi:web-asset'} />
+          </Link>
         </li>
       </ul>
     </div>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
