7a91e66d870f9fbf4c6e7bd3d84686ed143731e4
mc2 <themikechase@gmail.com>
2024-08-12 20:40:09 -0400

    button component to select plane on flask

--- backend/gis-risk/app/__init__.py
+++ backend/gis-risk/app/__init__.py
@@ -1,15 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask
+# from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+from .config import Config
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+
+    from .controller import map
+    app.register_blueprint(map.map_bp, url_prefix="/")
     return app
 
 app = create_app()
 
-@app.route("/")
-def index():
+# @app.route("/")
+# def index():
     # return "success"
-    return render_template("index.html")
+    # return render_template("index.html")
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", debug=True, port=5000, threaded=True)
+   app.run(host="0.0.0.0", debug=True, port=5000, threaded=True)
